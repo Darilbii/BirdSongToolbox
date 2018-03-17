d34eb35baab602ba779e8d330fd9308596a3bb5e
Darilbii <darilbii@gmail.com>
2018-03-16 23:31:50 -0400

    Added Import and Saving Functions for Probe Geometry

--- toolbox/geoprobe.py
+++ toolbox/geoprobe.py
@@ -126,3 +126,67 @@ def convert_probe_to_array(Probe_dict, Num_Chans):
             Probe_map[Channels[i], 0], Probe_map[Channels[i], 1] = Probe_dict[Channels[i]]
 
     return Probe_map
+
+# Function for saving Probe Geometry
+
+
+def save_Probe_Geometry(Probe_Geomtry, bird_id = str):
+    ''' Save Probe geometry using Pickle
+    
+    Parameters:
+    -----------
+    Probe_Geometry:  dict
+        Dictionary of the Probe Geometry
+        {Channel Number: tuple(X-Coordinate, Y-Coordinate)}
+        
+    bird_id: str
+        Bird Indentifier to Locate Specified Bird's data folder
+    
+    '''
+    # TODO: Add Assertion to Check if The File Already Exists
+    
+    desig_prb_file_folder = os.path.join('/net/expData/birdSong/', 'ss_data_Processed', bird_id)
+
+    file_Name = "probe_geometry"
+    file_path = os.path.join(desig_prb_file_folder, file_Name)
+
+    # open the file for writing
+    fileObject = open(file_path,'wb') 
+
+    # this writes the object a to the file named 'probe_geometry'
+    pickle.dump(Probe_Geometry, fileObject)   
+
+    # here we close the fileObject
+    fileObject.close()
+    
+# Function for Importing Probe Geometry
+
+def import_Probe_Geometry(bird_id = str):
+    ''' Import Probe Geomtry for Specified Bird
+    
+    Parameters:
+    -----------
+    bird_id: str
+        Bird Indentifier to Locate Specified Bird's data folder
+    
+    Returns:
+    --------
+    Probe_Geometry: dict
+        Dictionary of the Probe Geometry
+        {Channel Number: tuple(X-Coordinate, Y-Coordinate)}
+    
+    '''
+    # TODO: Add Assertion to Check if FIle Exists
+    
+    desig_prb_file_folder = os.path.join('/net/expData/birdSong/', 'ss_data_Processed', bird_id)
+
+    file_Name = "probe_geometry"
+    file_path = os.path.join(desig_prb_file_folder, file_Name)
+
+    # we open the file for reading
+    fileObject = open(file_path,'r')  
+    # load the object from the file into var b
+    Probe_Geometry = pickle.load(fileObject)  
+
+    fileObject.close()
+    return Probe_Geometry
