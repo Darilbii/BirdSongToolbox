124926d3a266d1ec0052b0c79e81aaef0a11ac45
Daril <debrown@ucsd.edu>
2018-10-29 14:16:57 -0700

    updated the Dyn_LFP_Clipper function and filled in some documentation on other functions

--- BirdSongToolbox/Epoch_Analysis_Tools.py
+++ BirdSongToolbox/Epoch_Analysis_Tools.py
@@ -140,6 +140,11 @@ def Get_LFP_Templates(Trials, Tr_Len, Gap_Len,  Buffer):
 def Full_Trial_LFP_Clipper_Old(Features, Sel_Motifs, SS=15, Low=5, Sel_Feature=2, Sliding=False, SUPP=False):
     ''' Grabs every epoch in Sel_Motifs for Each Frequency Band on Each Channel and returns them in a structure list
 
+
+    Information:
+    ------------
+        ** This Function must be run on the Train and Test Set Individually prior to continuing through the analysis**
+
     :param Features:
     :param Sel_Motifs:
     :param SS:
@@ -196,7 +201,11 @@ def Full_Trial_LFP_Clipper_Old(Features, Sel_Motifs, SS=15, Low=5, Sel_Feature=2
 
 
 def Full_Trial_LFP_Clipper(Neural, Sel_Motifs, Num_Freq, Num_Chan, Sn_Len, Gap_Len):
-    ''' Grabs every epoch in Sel_Motifs for Each Frequency Band on Each Channel and returns them in a structure list
+    """Grabs every epoch in Sel_Motifs for Each Frequency Band on Each Channel and returns them in a structure list
+
+    Information:
+    ------------
+        ** This Function must be run on the Train and Test Set Individually prior to continuing through the analysis**
 
     Parameters:
     -----------
@@ -218,19 +227,18 @@ def Full_Trial_LFP_Clipper(Neural, Sel_Motifs, Num_Freq, Num_Chan, Sn_Len, Gap_L
      Channel_Full_Freq_Trials: list
         [Ch]->[Freq]->(Time Samples x Trials)
 
-    '''
+    """
 
     # 1: Initiate Variables
 
     Channel_Full_Freq_Trials = []
 
     for Channel in range(Num_Chan):  # Over all Channels
-        Freq_Trials = []
         Freq_Full_Trials = []
         for Freq in range(Num_Freq):  # For Range of All Frequency Bins
             Chan_Full_Holder = np.zeros((Sn_Len + Gap_Len, 1))  # Initiate Holder for Trials (Motifs)
             for motif in Sel_Motifs:  # For each value of Sel_Motifs
-                Current_Full_Motif = Neural[motif - 1][Channel - 1][:, Freq]  # Select[Motif][Ch][Epoch, Freq]
+                Current_Full_Motif = Neural[motif - 1][Channel][:, Freq]  # Select[Motif][Ch][Epoch, Freq]
 
                 # Line Up all of the Selected Frequencies across All Trials for that Channel
                 Chan_Full_Holder = np.column_stack((Chan_Full_Holder, Current_Full_Motif))
@@ -355,7 +363,7 @@ def Label_Extract_Pipeline(Full_Trials, All_Labels, Time_Stamps, Label_Instructi
 # *** Check to Make sure the -1 in Select Motif stage is still accurate with current indexing ***
 
 def Numel(Index):
-    ''' Get the Number of Elements'''
+    """Get the Number of Elements"""
     Count = 0
     for i in range(len(Index)):
         Count = Count + (len(Index[i]))
@@ -363,6 +371,23 @@ def Numel(Index):
 
 
 def Numbad(Index, Offset=int, Tr_Length=int):
+    """ Returns the number of Label instances that finish after the end of the Epoch (Prevents Index errors propagating)
+
+    Parameters:
+    ----------
+    Index: list
+        A list of Lists containing the Start Times of only One type of Label in each Clipping.
+        Also Note that the Starts Argument must be converted to the 1 KHz Sampling Frequency
+
+    Offset: int
+
+    Tr_Length: int
+
+    return:
+    -------
+    count: int
+        number of bad indexes
+    """
     Count = 0
     for i in range(len(Index)):
         Count = Count + len([x for x in range(len(Index[i])) if Index[i][x] < (Offset + Tr_Length)])
@@ -370,14 +395,22 @@ def Numbad(Index, Offset=int, Tr_Length=int):
 
 
 def Numbad2(Index, ClipLen, Offset=int):
-    """Returns the number of Label instances that are past the edges of the Epoch (Prevents Index errors propagating)
+    """Returns the number of Label instances that start before the start of the Epoch (Prevents Index errors propagating)
 
-    :param Index:
-    :param ClipLen:
-    :param Offset:
-    :return:
+    Parameters:
+    ----------
+    Index: list
+        A list of Lists containing the Start Times of only One type of Label in each Clipping.
+        Also Note that the Starts Argument must be converted to the 1 KHz Sampling Frequency
+
+    ClipLen: int
+
+    Offset: int
+
+    return:
+    -------
     count: int
-        number of bacd indexes
+        number of bad indexes
     """
     Count = 0
     for i in range(len(Index)):
@@ -386,7 +419,8 @@ def Numbad2(Index, ClipLen, Offset=int):
 
 
 def Dyn_LFP_Clipper_Old(Features, Starts, Offset=int, Tr_Length=int):
-    ''' This Function Dynamically clips Neural data prior to a selected label and re-organizes them for future use.
+    """This Function Dynamically clips Neural data prior to a selected label and re-organizes them for future use.
+    (Prior Neural Data Only)
 
     Information:
     ------------
@@ -406,7 +440,7 @@ def Dyn_LFP_Clipper_Old(Features, Starts, Offset=int, Tr_Length=int):
     Returns:
     --------
 
-    '''
+    """
 
     ### Consider removing the Create_Bands Step and consider using len()
     ### ESPECIALLY IF YOU CHANGE THE BANDING CODE
@@ -445,15 +479,16 @@ def Dyn_LFP_Clipper_Old(Features, Starts, Offset=int, Tr_Length=int):
     return Dynamic_Freq_Trials, Dynamic_Templates
 
 
-def Dyn_LFP_Clipper(Features, Starts, Offset=int, Tr_Length=int):
-    ''' This Function Dynamically clips Neural data prior to a selected label and re-organizes them for future use.
+def Dyn_LFP_Clipper(Features: list, Starts, Offset=int, Tr_Length=int):
+    """This Function Dynamically clips Neural data prior to a selected label and re-organizes them for future use.
 
     Information:
     ------------
+        **This function assumes that the Data has Been limited to only Epoch that will be Trained or Tested**
         It iterates over EACH Epoch clipping ALL examples of ONE label in each trial.
         It should be run repeatedly for clipping all of the designated labels.
 
-        Its output can later be broken into an Initial Training and Final Test Set.
+        Its output can later be broken into an Initial Training and Final Test Set. [May Not Be True]
 
     Parameters:
     -----------
@@ -468,36 +503,42 @@ def Dyn_LFP_Clipper(Features, Starts, Offset=int, Tr_Length=int):
 
     Returns:
     --------
-
-    '''
+    Dynamic_Freq_Trials: list
+        List of Stacked Neural Data that corresponds to the Label designated
+        [Ch]->[Freq]->(Time (Samples) x Examples of Labels)
+        Note: The Number of Examples of Label does not always equal the total number of examples total as some push pass
+            the timeframe of the Epoch and are excluded
+    Dynamic_Templates: list
+        List of Stacked Template Neural Data that corresponds to the Label designated (Templates are the mean of trials)
+        [Ch]->[Freq]->(Time (Samples) x Examples of Labels)
+        Note: The Number of Examples of Label does not always equal the total number of examples total as some push past
+            the time frame of the Epoch and are excluded
+    """
 
     ### Consider removing the Create_Bands Step and consider using len()
     ### ESPECIALLY IF YOU CHANGE THE BANDING CODE
 
-    D = len(Features[:])  # Number of Channels
-    F = len(Features[0][:])  # Num of Frequency Bands
-    NT = len(Features[0][0][0, :])  # Number of Trials of Dynam. Clipped Training Set
-    NEl = Numel(Starts) - Numbad(Starts, Offset=Offset, Tr_Length=Tr_Length) - Numbad2(Starts, len(Features[0][0][:, 0]), Offset=Offset)  # Number of Examples
+    num_chan = len(Features[:])  # Number of Channels
+    freq_bands = len(Features[0][:])  # Num of Frequency Bands
+    num_trials = len(Features[0][0][0, :])  # Number of Trials of Dynam. Clipped Training Set
+    num_examples = Numel(Starts) - Numbad(Starts, Offset=Offset, Tr_Length=Tr_Length) - Numbad2(Starts, len(Features[0][0][:, 0]), Offset=Offset)  # Number of Examples
 
     Dynamic_Templates = []  # Index of all Channels
     Dynamic_Freq_Trials = []
-    for Channel in range(0, D):  # Over all Channels
+    for Channel in range(num_chan):  # Over all Channels
         Matches = []
         Freq_Trials = []
-        for l in range(0, F):  # For Range of All Frequency Bins
-            Chan_Holder = np.zeros((Tr_Length, NEl))  # Initiate Holder for Trials (Motifs)
-            Chan = Features[Channel - 1]  # Select Channel ##### Need to Change Channel to Channel Index (For For Loop)
-            Freq = Chan[l]
+        for l in range(0, freq_bands):  # For Range of All Frequency Bins
+            Chan_Holder = np.zeros((Tr_Length, num_examples))  # Initiate Holder for Trials (Motifs)
+            sel_freq_epochs = Features[Channel][l]
             Counter = 0  # For stacking all examples of label in full trial
-            for Trials in range(NT):
-                for Ex in range(len(Starts[Trials])):
-                    if Starts[Trials][Ex] - Offset - Tr_Length >= 0 and Starts[Trials][Ex] - Offset < len(Freq):
-                        if len(Freq[Starts[Trials][Ex] - Offset - Tr_Length:Starts[Trials][Ex] - Offset, Trials]) == 9:
-                            print(Starts[Trials][Ex] - Offset - Tr_Length)
-                            print(Starts[Trials][Ex] - Offset)  # Select Motif)
-                        Chan_Holder[:, Counter] = Freq[
-                                                  Starts[Trials][Ex] - Offset - Tr_Length:Starts[Trials][Ex] - Offset,
-                                                  Trials]  # Select Motif
+            for epoch in range(num_trials):
+                for example in range(len(Starts[epoch])):
+                    if Starts[epoch][example] - Offset - Tr_Length >= 0 and Starts[epoch][example] - Offset < len(sel_freq_epochs):
+                        # if len(sel_freq_epochs[Starts[epoch][example] - Offset - Tr_Length:Starts[epoch][example] - Offset, epoch]) == 9:
+                        #     print(Starts[epoch][example] - Offset - Tr_Length)
+                        #     print(Starts[epoch][example] - Offset)  # Select Motif)
+                        Chan_Holder[:, Counter] = sel_freq_epochs[Starts[epoch][example] - Offset - Tr_Length:Starts[epoch][example] - Offset, epoch]  # Select Motif
                         Counter = Counter + 1
             Freq_Trials.append(Chan_Holder)  # Save all of the Trials for that Frequency on that Channel
             Chan_Means = np.mean(Chan_Holder, axis=1)  # Find Means (Match Filter)
