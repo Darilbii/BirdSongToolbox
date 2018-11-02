a4039f6934a2491e18a67a93a269c903dbc8d408
Daril <debrown@ucsd.edu>
2018-11-02 14:26:04 -0700

    fixed index issue for when offset is negative again... sigh

--- BirdSongToolbox/Epoch_Analysis_Tools.py
+++ BirdSongToolbox/Epoch_Analysis_Tools.py
@@ -1290,9 +1290,9 @@ def Series_LFP_Clipper(Features, Offset: int, Tr_Length: int):
             for trials in range(nt):
                 for ex in range(len(starts[trials])):
                     if starts[trials][ex] - Offset - Tr_Length >= 0 and starts[trials][ex] - Offset <= len(Features[0][0][:, 0]) :
-                        if Offset<0:
+                        if Offset < 0:
                             chan_holder[:, counter] = frequency[
-                                                      starts[trials][ex] - Offset - Tr_Length:starts[trials][ex] - Offset - 1,
+                                                      starts[trials][ex] - Offset - Tr_Length:starts[trials][ex] - Offset + 1,
                                                       trials]  # Select Motif
 
                         else:
