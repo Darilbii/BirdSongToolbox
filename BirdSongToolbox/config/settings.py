8afccc09a11b6aa478e3be08b37348986c96917e
Daril <debrown@ucsd.edu>
2018-12-14 23:18:13 -0800

    Updated the /config/settings.py to use absolute paths for handling the local_config.pckl

--- BirdSongToolbox/config/settings.py
+++ BirdSongToolbox/config/settings.py
@@ -2,78 +2,18 @@
 import platform
 import pickle as pk
 from pathlib import Path
+import os
 
-# Check what computer is being used
-# if computer is one of the lab servers
-#     set the data path to be the corresponding data path
-# elif the computer isn't one of the lab computers
-#     check if their is a file called local_config.py
-#         if the file exists
-#             read the files contents and add them to the global scope
-#         else
-#             tell the user that they need to create this file
-#             ask if they want to make this file now
-#                 if yes
-#                      tell them that you are going to make this file now
-#                      ask the user what the data file path is for the local computer
-#                 if no
-#                     inform the user that the package will import but will not be able to import new data
 
 # TODO: handle paths for lintu and txori
 lab_servers = ['crunch', 'lintu', 'txori']
 lab_paths = {'crunch': '/net/expData/birdSong/'}  # Dictionary of Lab Servers
 
 HOSTNAME = platform.uname()[1]  # Get the identity of the host computer
-DEFAULT_PATH = Path('/config/local_config.pckl')
+# DEFAULT_PATH = Path('/config/local_config.pckl')
+DEFAULT_PATH = Path(__file__)
+DEFAULT_PATH = DEFAULT_PATH.parent / 'local_config.pckl'
 
-# DATA_PATH = get_data_path()
-
-# # for host in lab_servers:
-# #     if host in HOSTNAME:
-# #         DATA_PATH = lab_paths[host]
-#
-# if DATA_PATH in locals():
-#     # TODO:  Read settings file's contents and add them to the global scope
-#     pass
-# else:
-#     local_config = Path(DEFAULT_PATH)
-#     local_config.resolve()
-#     if local_config.exists():
-#         # import local path
-#         pass
-#     else:
-#         print('You are not currently working on a lab server and there is no local_config.py in your package')
-#         interacting = True
-#         while interacting:
-#             response = input('Would you like to create one now? (Y/N)')
-#             if response == 'y' or response == 'Y':
-#                 # TODO: make this a function call
-#                 make_config = True
-#                 local_location_path = Path(__file__)
-#                 create_local_config(local_location_path)
-#                 interacting = False
-#             elif response == 'n' or response == 'N':
-#                 make_config = False
-#                 print('Ok then...')
-#                 print('Note: Without a local_config.py this package will not be able to import data on this system')
-#                 interacting = False
-#             else:
-#                 response = input('Did not receive a Y or N. Would you like to create one now? (Y/N)')
-
-
-# def get_data_path():
-#     """ Determines the host computer being used and determines where the data directory is on the host
-#
-#     Returns:
-#     --------
-#     data_path: string
-#         File Path to the Birdsong Data to be imported
-#     """
-#
-#     if using_lab_server():
-#         return handle_lab_data_path()
-#     else:
-#         return handle_local_data_path()
 
 def using_lab_server():
     """ Check if using one of the lab servers """
@@ -92,8 +32,9 @@ def handle_lab_data_path():
 def handle_local_data_path():
     """ Function for handling package settings on non-lab computers"""
 
-    local_config = Path(DEFAULT_PATH)
-    local_config.resolve()
+    # local_config = Path(DEFAULT_PATH)
+    # local_config.resolve()
+    local_config = DEFAULT_PATH
 
     if local_config.exists():
         # import local path
@@ -137,7 +78,9 @@ def create_local_config():
     """
 
     # Make local Variables for while loop
-    default_path = Path(DEFAULT_PATH)
+    # default_path = Path(DEFAULT_PATH)
+    # default_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), DEFAULT_PATH)
+    default_path = DEFAULT_PATH
     making = True  # Currently Making Local Config File
     counter = 0  # Counts the number of attempts to verify the data path
 
@@ -170,10 +113,14 @@ def create_local_config():
 def load_local_data_path():
     """ Reads the local_config.pckl file and Loads the local Data path"""
 
-    default_path = Path(DEFAULT_PATH)
+    default_path = DEFAULT_PATH
+    # default_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'local_config.pckl')
+    # print(__file__)
+    # print(os.path.abspath(__file__))
 
     with default_path.open(mode='rb') as settings_file:
         local_path = pk.load(settings_file)
+        # local_path = ''
 
     return local_path
 
@@ -181,6 +128,13 @@ def load_local_data_path():
 # Main Function for Determining Settings on Import
 def main():
     """ Determines the host computer being used and determines where the data directory is on the host"""
+    print(__file__)
+    print(os.path.dirname(os.path.abspath(__file__)))
+
+    test = Path(__file__)
+    print(test.resolve())
+    print(test.exists())
+
     if using_lab_server():
         DATA_PATH = handle_lab_data_path()
     else:
