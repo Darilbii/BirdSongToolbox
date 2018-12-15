6ebebf7c425c31b2806398a49d9b57bcb1046038
Daril <debrown@ucsd.edu>
2018-12-14 17:50:02 -0800

    Updated the gitignores and the settings handling

--- BirdSongToolbox/__init__.py
+++ BirdSongToolbox/__init__.py
@@ -1,5 +1,7 @@
 from .ImportClass import *
 from .PreProcessClass import *
 from .PreProcTools import *
+import BirdSongToolbox.config
+
 # TODO: Once birdsong_renovated Remove this comment
 from .birdsong_renovated import *
