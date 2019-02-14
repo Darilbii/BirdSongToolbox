8cf6dd4bc102fbccdb30d410a97a2cb8af9f1a93
Daril <debrown@ucsd.edu>
2019-02-14 13:26:05 -0800

    created a setup.;y file and begun version tracking

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BirdSongToolbox',
+    version='0.1.0dev',
+    packages=['', 'BirdSongToolbox'],
+    url='',
+    license='',
+    author='Daril Brown II',
+    author_email='',
+    description=''
+)
