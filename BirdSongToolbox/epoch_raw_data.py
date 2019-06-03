d0b59996293cf06a3a135d03a47bc619a5a326d2
Daril <debrown@ucsd.edu>
2019-06-03 15:31:50 -0700

    Serious Improvements to the determine_chunks_for_epoch to handle edge cases and give the information needed

--- BirdSongToolbox/epoch_raw_data.py
+++ BirdSongToolbox/epoch_raw_data.py
@@ -14,42 +14,68 @@ def determine_chunks_for_epochs(times):
     -------
     logged : array
         array of the automated motif labels to use as benchmarks for the long epochs
+    ledger : list
+        list of each automated motif that occurs within each Epoch
+        [Chunk (Epoch)] -> [Motifs in Epoch (Chunk)]
     """
-    times_sorted = np.argsort(times)
-    ledger = list(times_sorted[1:])
+    times_sorted = np.argsort(times)  # Sort the Motif Starts in Sequential Order
+    roster = list(times_sorted[1:])  # Make a roster of the motifs in sequential order excluding the first
+    ledger = []
     logged = []
-    focus = times_sorted[0]  # Select the second Candidate for the End of the Chunk
-    candidate = times_sorted[1]  # Select the First Candidate for the End of the Chunk
+    focus = times_sorted[0]  # Select the first Start motif focus
+    candidate = focus  # Select the First Candidate for the End of the Chunk
+    ledger_focus = [focus]
     counter = 0
 
-    while len(ledger) > 0:
-        competitor = ledger[0]  # Select the Candidate for the End of the Chunk
-        distance = (times[competitor] - times[focus]) / 30000  # Distance in Seconds
+    while len(roster) > 0:
+        print(len(roster))
+        competitor = roster[0]  # Select the Candidate for the End of the Chunk
+        distance = (times[competitor] - times[candidate]) / 30000  # Distance in Seconds
 
-        if distance < 2:
-            #         print(f"if: {distance}")
-            candidate = ledger.pop(0)  # Update with the new candidate
+        # The Last Motif for the Entire Recording
+        if len(roster) == 1:
+            if counter > 0:
+                ledger_focus.extend([competitor])  # Add the competitor to this Chunk's Ledger
+                logged.append([focus, competitor])  # Save the Start and End of the Successful Chunk Pair
+                ledger.append(ledger_focus)  # Append the Chunk's Completed Ledger to the Overall Ledger
+                roster.pop(0)  # Close out the While Loop
+            else:
+                logged.append([focus, None])  # First Special Case Lone Motif
+                ledger.append(ledger_focus)  # Append the Chunk's Completed Ledger to the Overall Ledger
+                logged.append([competitor, None])  # Second Special Case Lone Motif (Nothing After It)
+                ledger_focus = [competitor]  # Create new Ledger for the Next Chunk
+                ledger.append(ledger_focus)  # Append the Chunk's Completed Ledger to the Overall Ledger
+                roster.pop(0)  # Close out the While Loop
+
+        # From the Starting Motif to the Second to Last Motif
+        elif distance < 2:
+            ledger_focus.extend([competitor])  # Add the competitor to this Chunk's Ledger
+            candidate = roster.pop(0)  # Update to the new candidate
             counter = counter + 1  # Count the Number of Motifs Contained in the Motif
 
         elif distance < 30:
-            #         print(f"elif: {distance}")
-            candidate = ledger.pop(0)  # Update with the new candidate
+            ledger_focus.extend([competitor])  # Add the competitor to this Chunk's Ledger
+            candidate = roster.pop(0)  # Update to the new candidate
             counter = counter + 1  # Count the Number of Motifs Contained in the Motif
 
         else:
-            #         print(f"else: {distance}")
-            print(counter)
             if counter > 0:
-                logged.append([focus, candidate])  # Save the Start and End of the Sucessful Chunk Pair
-                focus = competitor  # Update the New Start Focus to the competitor
-                ledger.pop(0)  # Clear the roster for the New Candidate
-                candidate = ledger.pop(0)  # Update the New Start Candidated
+                logged.append([focus, candidate])  # Save the Start and End of the Successful Chunk Pair
+                focus = competitor  # Update the Focus to the competitor [New Starting Motif]
+                roster.pop(0)  # Clear the roster for the New Competitor
+                ledger.append(ledger_focus)  # Append the Chunk's Completed Ledger to the Overall Ledger
+                ledger_focus = [focus]  # Create new Ledger for the Next Chunk
+                candidate = focus  # Update the New Start Candidate and remove them from the Roster
                 counter = 0
             else:
                 logged.append([focus, None])  # Special Case Lone Motif
                 focus = competitor  # Update the New Start Focus to the competitor
-                ledger.pop(0)  # Clear the roster for the New Candidate
-                candidate = ledger.pop(0)  # Update the New Start Candidated
+                roster.pop(0)  # Clear the Special Lone Motif from the roster
+                ledger.append(ledger_focus)  # Append the Chunk's Completed Ledger to the Overall Ledger
+                ledger_focus = [focus]  # Create new Ledger for the Next Chunk
+                candidate = focus  # Update the New Start Candidate
                 counter = 0
 
-    return logged
+    return logged, ledger
+
+
