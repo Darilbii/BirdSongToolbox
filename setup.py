eca9978ee35a98ea086b2bbcf2c9debc3e9cbaa2
Daril <debrown@ucsd.edu>
2019-06-04 11:24:38 -0700

    updated the requirements and setup to have mne as a optional dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+"""
+===============
+BirdSongToolbox
+===============
+
+"""
 
 setup(
     name='BirdSongToolbox',
     version='0.1.0dev',
-    packages=['BirdSongToolbox'],
+    packages=find_packages(),
     url='',
     license='',
     author='Daril Brown II',
     author_email='',
-    description=''
+    description='',
+    install_requires=['numpy', 'scipy', 'matplotlib', 'pandas', 'h5py'],
+    keywords=['neuroscience', 'neural oscillations', 'bird song', 'spectral analysis', 'LFP'],
+    tests_require=['pytest'],
+    extra_requires={
+        'mne': ['mne']
+    }
 )
