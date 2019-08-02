482c4c583fc4672d5d97752249aaf854960eaf1b
Daril <debrown@ucsd.edu>
2019-08-01 18:41:24 -0700

    removed the birdsong_renovated.py from the __init__.py of the BirdSongToolbox Module

--- BirdSongToolbox/__init__.py
+++ BirdSongToolbox/__init__.py
@@ -4,4 +4,4 @@ from .PreProcessClass import *
 from .PreProcTools import *
 
 # TODO: Once birdsong_renovated Remove this comment
-from .birdsong_renovated import *
+# from .birdsong_renovated import *
