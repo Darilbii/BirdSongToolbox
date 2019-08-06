d337f25bf95ec3d06fffd96a589588ff9a2f6e38
Daril <debrown@ucsd.edu>
2019-08-06 11:07:01 -0700

    transfered over the new label extractor functions and created tests to cover each of them using the main function and parameterize

--- BirdSongToolbox/free_epoch_tools.py
+++ BirdSongToolbox/free_epoch_tools.py
@@ -36,3 +36,102 @@ def get_chunk_handlabels(handlabels_list):
     onsets_list = [starts_list, ends_list]
 
     return labels_list, onsets_list
+
+
+def label_focus_chunk(focus, labels, starts):
+    """ Create a list of every instance of the User defined User Label (Focus on One Label)
+
+    Parameters
+    ----------
+    focus : str or int
+        User defined Label to focus on
+    labels : list
+        List of all Labels corresponding to each Epoch in Full_Trials
+        [Epochs]->[Labels]
+    starts : list
+        List of all Start Times corresponding to each Epoch in Full_Trials
+        [Epochs]->[Start Time]
+
+    Returns
+    -------
+    label_index : list
+        List of all start frames of every instances of the label of focus
+        [Num_Trials]->[Num_Exs]
+    """
+    label_index = []
+
+    # for i in range(len(labels)):
+    #     Trial_Labels = [int(starts[i][x] / 30) for x in range(len(labels[i])) if labels[i][x] == Focus]
+
+    for start, epoch in zip(starts, labels):
+        trial_labels = [start[i] for i, x in enumerate(epoch) if x == focus]
+        label_index.append(trial_labels)
+    return label_index
+
+
+# Function for Grouping Multiple Labels into 1 Label (e.g. Combine Calls and Introductory Notes)
+
+def label_group_chunk(Focuses, labels, starts):
+    """Group Selected labels together into One Label e.g. Combine Calls and Intro. Notes (Group these labels together)
+
+    Parameters
+    ----------
+    focus : str or int
+        User defined Label to focus on
+    labels : list, shape [Epochs]->[labels]
+        List of all labels corresponding to each Epoch in Full_Trials
+    starts : list, shape [Epochs]->[Start Time]
+        List of all Start Times corresponding to each Epoch in Full_Trials
+
+    Returns
+    -------
+    label_index : list, shape [Num_Trials]->[Num_Exs]
+        List of all start frames of every instances of the label of focus
+    """
+
+    label_index = []
+
+    for start, chunk in zip(starts, labels):
+        trial_labels = [start[index] for index, label in enumerate(chunk) if label in Focuses]
+        label_index.append(trial_labels)
+    return label_index
+
+
+# Function for grabing more examples from a onset
+
+
+def label_extractor(all_labels, starts, label_instructions):
+    """Extracts all of the Neural Data Examples of User Selected Labels and return them in the designated manner.
+
+    Label_Instructions = tells the Function what labels to extract and whether to group them together
+
+    Parameters
+    ----------
+    all_labels : list, shape [Epochs]->[Labels]
+        List of all Labels corresponding to each Epoch in Full_Trials
+    starts : list, shape [Epochs]->[Start Time]
+        List of all Start Times corresponding to each Epoch in Full_Trials
+    label_instructions : list
+        list of labels and how they should be treated. If you use a nested list in the instructions the labels in
+        this nested list will be treated as if they are the same label
+
+    Returns
+    -------
+    specified_labels : list, shape [Labels]->[Chunk]->[Times]
+        List containing the instances of the Labels for each Chunk
+
+    """
+
+    specified_labels = []
+
+    for instruction in range(len(label_instructions)):
+        if type(label_instructions[instruction]) == int or type(label_instructions[instruction]) == str:
+            label_starts = label_focus_chunk(label_instructions[instruction], all_labels, starts)
+        else:
+            label_starts = label_group_chunk(label_instructions[instruction], all_labels, starts)
+        specified_labels.append(label_starts)
+
+    # if len(specified_labels) == 1:
+    #     specified_labels = specified_labels[0]
+
+    return specified_labels
--- BirdSongToolbox/test/test_free_epoch_tools.py
+++ BirdSongToolbox/test/test_free_epoch_tools.py
@@ -2,7 +2,7 @@
 
 import pytest
 from BirdSongToolbox.file_utility_functions import _load_pckl_data
-from BirdSongToolbox.free_epoch_tools import get_chunk_handlabels
+from BirdSongToolbox.free_epoch_tools import get_chunk_handlabels, label_extractor
 
 
 @pytest.mark.run(order=1)
@@ -25,6 +25,20 @@ def chunk_handlabels(bird_id, session, chunk_data_path):
                            source=data_path)
 
 @pytest.mark.run(order=1)
+@pytest.fixture()
+def onsets_testcase(chunk_handlabels):
+    chunk_labels_list, chunk_onsets_list = get_chunk_handlabels(handlabels_list=chunk_handlabels)
+    return chunk_onsets_list
+
+
+@pytest.mark.run(order=1)
+@pytest.fixture()
+def labels_testcase(chunk_handlabels):
+    chunk_labels_list, chunk_onsets_list = get_chunk_handlabels(handlabels_list=chunk_handlabels)
+    return chunk_labels_list
+
+
+@pytest.mark.run(order=1)
 def test_get_chunk_handlabels(chunk_handlabels):
     chunk_labels_list, chunk_onsets_list = get_chunk_handlabels(handlabels_list=chunk_handlabels)
 
@@ -33,6 +47,15 @@ def test_get_chunk_handlabels(chunk_handlabels):
     assert len(chunk_onsets_list) == 2
     assert len(chunk_onsets_list[0]) == len(chunk_onsets_list[1]) == len(chunk_labels_list)
 
+
+@pytest.mark.run(order=1)
+@pytest.mark.parametrize("test_instructions", [[1, 2, 3, 4], [1, [2, 3], 4], [1], [1, 'C', 'I'], ['BUFFER']])
+def test_label_extractor(labels_testcase, onsets_testcase, test_instructions):
+
+    specified_labels = label_extractor(all_labels=labels_testcase, starts=onsets_testcase[0],
+                                       label_instructions=test_instructions)
+
+    assert len(specified_labels) == len(test_instructions)
 
 
 
