6b6436abf12c5a5c89292c8fe5d2cd28cc3d25be
Daril <debrown@ucsd.edu>
2019-08-13 15:36:57 -0700

    wrote smoke tests for the hilbert_module in the preprocess module

--- BirdSongToolbox/test/test_preprocess.py
+++ BirdSongToolbox/test/test_preprocess.py
@@ -1,7 +1,7 @@
 import pytest
 from BirdSongToolbox.file_utility_functions import _load_numpy_data, _load_pckl_data
 from BirdSongToolbox.preprocess import common_average_reference_array, common_average_reference, bandpass_filter, \
-    bandpass_filter_array_mne, bandpass_filter_array_ndsp, bandpass_filter_epochs, multi_bpf_epochs
+    bandpass_filter_array_mne, bandpass_filter_array_ndsp, bandpass_filter_epochs, multi_bpf_epochs, hilbert_module
 
 @pytest.mark.run(order=1)
 @pytest.fixture()
@@ -71,3 +71,21 @@ def test_multi_bpf_epochs(chunk_neural_data):
 
     filt_data = multi_bpf_epochs(epoch_neural_data=chunk_neural_data, fs=fs, l_freqs=l_freqs, h_freqs=h_freqs,
                                  remove_edges=False, verbose=False)
+
+@pytest.mark.run(order=1)
+@pytest.mark.parametrize("output,smooth", [("amplitude", False),
+                                           ("phase", False),
+                                           ("phase", True),
+                                           pytest.param("Wrong", False, marks=pytest.mark.xfail),
+                                           pytest.param(False, False, marks=pytest.mark.xfail)])
+def test_hilbert_module(chunk_neural_data, output, smooth):
+    fs = 1000
+    l_freq = 10
+    h_freq = 20
+
+    filt_data = bandpass_filter(neural_data=chunk_neural_data[0], fs=fs, l_freq=l_freq, h_freq=h_freq)
+    hilbert_module(neural_data=filt_data, output=output, smooth=smooth)
+
+
+
+
