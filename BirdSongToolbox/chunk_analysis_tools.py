80e79f6360fef3bc0b052f968f1ea44fd8a69a48
Daril Brown <darilbii@gmail.com>
2019-12-02 19:58:02 -0800

    updated a slew of the functions and re-wrote a more efficient random single channel feature dropping function

--- BirdSongToolbox/chunk_analysis_tools.py
+++ BirdSongToolbox/chunk_analysis_tools.py
@@ -2,7 +2,9 @@
 
 import numpy as np
 import scipy
+import random
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def make_templates(event_data):
@@ -24,7 +26,7 @@ def make_templates(event_data):
     return np.array(templates)
 
 
-def ml_selector(event_data, identity_index, label_index, sel_instances, make_template=False, verbose=False):
+def ml_selector(event_data, identity_index, label_index, sel_instances):
     """ Collects Instances of Interest from the event_data
 
     Parameters
@@ -37,11 +39,6 @@ def ml_selector(event_data, identity_index, label_index, sel_instances, make_tem
         array of labels that indicates the class that instance is an example of
     sel_instances : ndarray | (number_instances_total,)
         array of indexes that represent the individual indexes of all instances across class labels
-
-    make_template : bool
-        If True function will return the mean of all instances for each label's Channel/Frequency pair, defaults to False
-    verbose: bool
-        If True the function will print out useful info about its progress, defaults to False
 
     Returns
     -------
@@ -208,12 +205,12 @@ def find_pearson_coeff(cl_data, templates, slow=False):
     return corr_trials
 
 
-def pearson_extraction(clipped_data, templates):
+def pearson_extraction(event_data, templates):
     """  Pearson Correlation Coefficients for all Labels
 
     Parameters
     ----------
-    clipped_data : ndarray | (labels, instances, frequencies, channels, samples)
+    event_data : ndarray | (labels, instances, frequencies, channels, samples)
         Array containing all the neural segments for all labels.
         (As defined by Label_Instructions in label_extract_pipeline)
     templates : ndarray | (labels, frequencies, channels, samples)
@@ -225,49 +222,72 @@ def pearson_extraction(clipped_data, templates):
         Array of Pearson Correlation Values between each instance and the LFP Template of each Label
     """
     extracted_pearson = []
-    for label in clipped_data:
+    for label in event_data:
         extracted_pearson.append(find_pearson_coeff(label, templates=templates))
     return np.asarray(extracted_pearson)
 
 
-def make_channel_identity_ledger(number_frequencies, number_channels, number_templates):
-    """ Make a Feature Identity Ledger for One type of Feature rows=[Freqs, Channels] | (2, Num_Features)"""
+def make_feature_id_ledger(num_freqs, num_chans, num_temps):
+    """ Make a Feature Identity Ledger for One type of Feature rows=[Freqs, Channels] | (2, Num_Features)
 
-    if number_templates:
-        entries_ledger = np.zeros((3, number_frequencies, number_channels, number_templates))
+    Parameters
+    ----------
+    num_freqs : int
+        the number of frequency bands
+    num_chans : int
+        the number of recording channels
+    num_temps : int, optional
+        the number of pearson templates, only include if the feature type is pearson
+
+    Returns
+    -------
+    entries_ledger : ndarray | (num_total_features, [frequencies, channels, templates])
+        ledger of the feature identity for the scikit-learn data structure
+
+    """
+
+    if num_temps:
+        entries_ledger = np.zeros((3, num_freqs, num_chans, num_temps))
         # Make Ledger for Frequency
-        entries_ledger[0] = entries_ledger[0] + np.arange(number_frequencies)[:, None, None]  # Index of Freq
+        entries_ledger[0] = entries_ledger[0] + np.arange(num_freqs)[:, None, None]  # Index of Freq
         # Make Ledger for Channel
-        entries_ledger[1] = entries_ledger[1] + np.arange(number_channels)[None, :, None]  # Index of Channel
+        entries_ledger[1] = entries_ledger[1] + np.arange(num_chans)[None, :, None]  # Index of Channel
         # Make Ledger for templates
-        entries_ledger[2] = entries_ledger[2] + np.arange(number_templates)[None, None, :]  # Index of Templates
+        entries_ledger[2] = entries_ledger[2] + np.arange(num_temps)[None, None, :]  # Index of Templates
         entries_ledger = np.asarray(entries_ledger)  # Convert to ndarray
         entries_ledger = entries_ledger.reshape((3, -1))  # Convert (3, Num_Features)
 
     else:
-        entries_ledger = np.zeros((2, number_frequencies, number_channels))
+        entries_ledger = np.zeros((2, num_freqs, num_chans))
         # Make Ledger for Frequency
-        entries_ledger[0, :, :] = entries_ledger[0, :, :] + np.arange(number_frequencies)[:, None]  # Index of Freq
+        entries_ledger[0, :, :] = entries_ledger[0, :, :] + np.arange(num_freqs)[:, None]  # Index of Freq
         # Make Ledger for Channel
-        entries_ledger[1, :, :] = entries_ledger[1, :, :] + np.arange(number_channels)[None, :]  # Index of Channel
+        entries_ledger[1, :, :] = entries_ledger[1, :, :] + np.arange(num_chans)[None, :]  # Index of Channel
         entries_ledger = np.asarray(entries_ledger)  # Convert to ndarray
         entries_ledger = entries_ledger.reshape((2, -1))  # Convert (2, Num_Features)
 
-    return entries_ledger
+    return np.transpose(entries_ledger)
 
 
-# TODO: Test This
-def ml_order(extracted_pearson):
+def ml_order(extracted_features_array):
     """
     Parameters
     ----------
-    extracted_pearson : ndarray | (labels, instances, frequencies, channels, templates)
+    extracted_features_array : ndarray | (labels, instances, frequencies, channels, templates)
         Array of Pearson Correlation Values between each instance and the LFP Template of each Label
 
+    Returns
+    -------
+    ordered_trials : ndarray | (n_samples, n_features)
+        Data array that is structured to work with the SciKit-learn Package
+            n_samples = Num of Instances Total
+            n_features = Num_Ch * Num_Freq)
+    ml_labels : ndarray | (n_training_samples, )
+        1-d array of Labels of the ordered_trials instances )(n_samples)
     """
     ml_labels = []
     ordered_trials = []
-    for index, label in enumerate(extracted_pearson):
+    for index, label in enumerate(extracted_features_array):
         # Machine Learning Data
         num_instances = len(label)
         reshaped = np.reshape(label, (num_instances, -1))
@@ -279,9 +299,75 @@ def ml_order(extracted_pearson):
         ml_labels.extend(label_dummy)
 
     ordered_trials = np.array(ordered_trials)
-    ml_labels = np.array(ml_labels)
+    ml_labels = np.array(ml_labels)[:, 0]
 
     return ordered_trials, ml_labels
+
+
+def make_feature_dict(ordered_index, drop_type: str):
+    """Creates a Dictionary of the the indexes for each Channel's features in the ordered_index
+
+    Parameters
+    ----------
+    ordered_index : ndarray | (num_total_features, [frequencies, channels, templates])
+        ledger of the feature identity for the scikit-learn data structure
+    drop_type : str
+        Controls whether the dictionary indexes the channel number of the frequency band
+    Returns
+    -------
+    feature_dict : dict | {feature: [list of Indexes]}
+        dictionary to be used to remove all features for either a single channel or frequency band
+
+    """
+    options = ['channel', 'frequency']
+    assert drop_type in options
+
+    if drop_type == 'frequency':
+        sel = 0
+    elif drop_type == 'channel':
+        sel = 1
+
+    ordered_index_shape = np.max(ordered_index, axis=0) + 1
+    sel_len = int(ordered_index_shape[sel])
+
+    feature_dict = {}
+    for i in range(sel_len):
+        feature_dict[i] = [index for index, description in enumerate(ordered_index) if description[sel] == i]
+
+    return feature_dict
+
+
+def drop_features(features, keys, desig_drop_list):
+    """Function for Selectively Removing Columns for Feature Dropping
+
+    Parameters
+    ----------
+    features : ndarray | (n_samples, n_features)
+        Data array that is structured to work with the SciKit-learn Package
+            n_samples = Num of Instances Total
+            n_features = Num_Ch * Num_Freq)
+    keys : dict | {feature: [list of Indexes]}
+        dictionary to be used to remove all features for either a single channel or frequency band
+    desig_drop_list : list
+        list of features to be dropped
+
+    Returns
+    -------
+    remaining_features : ndarray | (n_samples, n_features_remaining)
+        Data array that is structured to work with the SciKit-learn Package
+    full_drop : list
+        list of list of all Features (columns) to be dropped
+
+    """
+    # flatten_matrix = [val
+    #                   for sublist in matrix
+    #                   for val in sublist]
+
+    full_drop = [val for i in desig_drop_list for val in keys[i]]  # Store the Index of Features to be dropped
+
+    remaining_features = np.delete(features, full_drop, axis=1)
+
+    return remaining_features, full_drop
 
 
 def clip_classification(ClassObj, train_set, train_labels, test_set, test_labels):
@@ -328,4 +414,205 @@ def clip_classification(ClassObj, train_set, train_labels, test_set, test_labels
 
     return acc, classifier, confusion
 
+
+def random_feature_dropping(train_set: np.ndarray, train_labels: np.ndarray, test_set: np.ndarray,
+                            test_labels: np.ndarray, ordered_index, drop_type,  Class_Obj, verbose=False):
+    """ Repeatedly trains/test models to create a feature dropping curve (Originally for Pearson Correlation)
+
+    Parameters
+    ----------
+    train_set : ndarray | (n_samples, n_features)
+        Training data array that is structured to work with the SciKit-learn Package
+    train_labels : ndarray | (n_training_samples, )
+        1-d array of Labels of the Corresponding n_training_samples
+    test_set : ndarray  | (n_samples, n_features)
+        Testing data Array that is structured to work with the SciKit-learn Package
+    test_labels : ndarray | | (n_training_samples, )
+        1-d array of Labels of the Corresponding n_test_samples
+    ordered_index : ndarray | (num_total_features, [frequencies, channels, templates])
+        ledger of the feature identity for the scikit-learn data structure
+            Power:   (Num of Features, [frequencies, channels])
+            Pearson: (Num of Features, [frequencies, channels, templates])
+    drop_type : str
+        Controls whether the dictionary indexes the channel number of the frequency band
+    Class_Obj : class
+        classifier object from the scikit-learn package
+    verbose : bool
+        If True the funtion will print out useful information for user as it runs, defaults to False.
+
+    Returns
+    -------
+    dropping_curve : ndarray
+        ndarray of accuracy values from the feature dropping code (values are floats)
+        (Number of Features (Decreasing), Number of Nested Folds)
+    """
+
+    # 1.) Initiate Lists for Curve Components
+    feat_ids = make_feature_dict(ordered_index=ordered_index, drop_type=drop_type)  # Convert ordered_index to a dict
+    num_channels = len(feat_ids.keys())  # Determine the Number of Dropping indexes
+    dropping_curve = np.zeros([num_channels + 1, 1])  # Create Empty array for Dropping Curves
+    drop_list = []
+
+    # 2.) Print Information about the Feature Set to be Dropped
+    if verbose:
+        print("Number of columns dropped per cycle", len(feat_ids[0]))  # Print number of columns per dropped feature
+        print("Number of Channels total:", len(feat_ids))  # Print number of Features
+
+    temp = feat_ids.copy()  # Create a temporary internal *shallow? copy of the index dictionary
+
+    # 3.) Begin Feature Dropping steps
+    # Find the first Accuracy
+
+    first_acc, _, _ = clip_classification(ClassObj=Class_Obj, train_set=train_set, train_labels=train_labels,
+                                          test_set=test_set, test_labels=test_labels)
+
+    if verbose:
+        print("First acc: %s..." % first_acc)
+        # print("First Standard Error is: %s" % first_err_bars)  ###### I added this for the error bars
+
+    dropping_curve[0, :] = first_acc  # Append BDF's Accuracy to Curve List
+    index = 1
+
+    while num_channels > 2:  # Decrease once done with development
+        ids_remaining = list(temp.keys())  # Make List of the Keys(Features) from those that remain
+        num_channels = len(ids_remaining)  # keep track of the number of Features
+        # Select the index for Feature to be Dropped from list of keys those remaining (using random.choice())
+        drop_feat_ids = random.choice(ids_remaining)
+
+        if verbose:
+            print("List of Channels Left: ", ids_remaining)
+            print("Number of Channels Left:", num_channels)
+            print("Channel to be Dropped:", drop_feat_ids)
+
+        # Remove Key and Index for Designated Feature
+        del temp[drop_feat_ids]  # Delete key for Feature Designated to be Dropped from overall list
+
+        drop_list.append(drop_feat_ids)  # Add Designated Drop Feature to Drop list
+
+        # Remove sel feature from train feature array
+        train_remaining_features, _ = drop_features(features=train_set, keys=feat_ids, desig_drop_list=drop_list)
+
+        # Remove sel feature from test feature array
+        test_remaining_features, _ = drop_features(features=test_set, keys=feat_ids, desig_drop_list=drop_list)
+
+        acc, _, _ = clip_classification(ClassObj=Class_Obj, train_set=train_remaining_features,
+                                        train_labels=train_labels, test_set=test_remaining_features,
+                                        test_labels=test_labels)
+
+        dropping_curve[index, :] = acc  # Append Resulting Accuracy to Curve List
+
+        if verbose:
+            print("Drop accuracies: ", acc)
+            print("Dropping Feature was %s..." % drop_feat_ids)
+
+        index += 1
+
+    return dropping_curve
+
+
+def random_feature_drop_multi_narrow_chunk(event_data, ClassObj, k_folds=5, seed=None, verbose=False):
+    """ Runs the Random Channel Feature Dropping algorithm on a set of pre-processed data
+
+    Parameters
+    ----------
+    event_data : ndarray | (classes, instances, frequencies, channels, samples)
+        Randomly Rebalanced Neural Data (output of balance_classes)
+    ClassObj : class
+        classifier object from the scikit-learn package
+    k_folds : int
+        Number of Folds to Split between Template | Train/Test sets, defaults to 5,
+    seed : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator; If RandomState instance, random_state is
+        the random number generator; If None, the random number generator is the RandomState instance used by np.random.
+    verbose : bool
+        If True the funtion will print out useful information for user as it runs, defaults to False.
+
+    Returns
+    -------
+    """
+
+    # 1.) Make Array for Holding all of the feature dropping curves
+    nested_dropping_curves = []  # np.zeros([])
+
+    # 2.) Create INDEX of all instances of interests : create_discrete_index()
+    label_identities, label_index = create_discrete_index(event_data=event_data)
+    identity_index = np.arange(len(label_index))
+    sss = StratifiedShuffleSplit(n_splits=k_folds, random_state=seed)
+    sss.get_n_splits(identity_index, label_index)
+
+    if verbose:
+        print(sss)
+
+    # --------- For Loop over possible Training Sets---------
+    for train_index, test_index in sss.split(identity_index, label_index):
+        if verbose:
+            print("TRAIN:", train_index, "TEST:", test_index)
+
+        X_train, X_test = identity_index[train_index], identity_index[test_index]
+        y_train, y_test = label_index[train_index], label_index[test_index]
+
+        # 4.) Use INDEX to Break into corresponding [template/training set| test set] : ml_selector()
+        # 4.1) Get template set/training : ml_selector(event_data, identity_index, label_index, sel_instances)
+        sel_train = ml_selector(event_data=event_data, identity_index=label_identities, sel_instances=X_train,
+                                label_index=y_train)
+
+        # 4.1) Get test set : ml_selector()
+        sel_test = ml_selector(event_data=event_data, identity_index=label_identities, sel_instances=X_test,
+                               label_index=y_test)
+
+        # 5.) Use template/training set to make template : make_templates(event_data)
+        templates = make_templates(event_data=event_data)
+
+        # 6.1) Use template/training INDEX and template to create Training Pearson Features : pearson_extraction()
+        train_pearson_features = pearson_extraction(event_data=sel_train, templates=templates)
+
+        # 6.2) Use test INDEX and template to create Test Pearson Features : pearson_extraction()
+        test_pearson_features = pearson_extraction(event_data=sel_test, templates=templates)
+
+        # 7.1) Reorganize Test Set into Machine Learning Format : ml_order_pearson()
+        ml_trials_train, ml_labels_train = ml_order(extracted_features_array=train_pearson_features)
+
+        # 7.2) Get Ledger of the Features
+        num_freqs, num_chans, num_temps = np.shape(train_pearson_features[0][0])  # Get the shape of the Feature data
+        ordered_index = make_feature_id_ledger(num_freqs=num_freqs, num_chans=num_chans, num_temps=num_temps)
+
+        # 7.3) Reorganize Training Set into Machine Learning Format : ml_order_pearson()
+        ml_trials_test, ml_labels_test = ml_order(extracted_features_array=test_pearson_features)
+
+        fold_frequency_curves = []
+        for freq in range(num_freqs):
+            if verbose:
+                print("On Frequency Band:", freq, " of:", num_freqs)
+
+            ml_trials_train_cp = ml_trials_train.copy()  # make a copy of the feature extracted Train data
+            ml_trials_test_cp = ml_trials_test.copy()  # make a copy of the feature extracted Test data
+            ordered_index_cp = ordered_index.copy()  # make a copy of the ordered_index
+            all_other_freqs = list(np.delete(np.arange(num_freqs), [freq])) # Make a index of the other frequencies
+            temp_feature_dict = make_feature_dict(ordered_index=ordered_index_cp, drop_type='frequency')  # Feature Dict
+            # reduce to selected frequency from the COPY of the training data
+            ml_trials_train_freq, full_drop = drop_features(features=ml_trials_train_cp, keys=temp_feature_dict,
+                                                 desig_drop_list=all_other_freqs)
+            # reduce to but the selected frequency from the COPY of test data
+            ml_trials_test_freq, _ = drop_features(features=ml_trials_test_cp, keys=temp_feature_dict,
+                                                desig_drop_list=all_other_freqs)
+            ordered_index_cp = np.delete(ordered_index_cp, full_drop, axis=0)  # Remove features from other frequencies
+
+            # 8.) Perform Nested Feature Dropping with K-Fold Cross Validation
+            nested_drop_curve = random_feature_dropping(train_set=ml_trials_train_freq, train_labels=ml_labels_train,
+                                                        test_set=ml_trials_test_freq, test_labels=ml_labels_test,
+                                                        ordered_index=ordered_index_cp, drop_type='channel',
+                                                        Class_Obj=ClassObj, verbose=False)
+            fold_frequency_curves.append(nested_drop_curve)
+
+        nested_dropping_curves.append(fold_frequency_curves)
+
+    # 9.) Combine all curve arrays to one array
+    all_drop_curves = np.array(nested_dropping_curves)  # (folds, frequencies, num_dropped, 1)
+
+
+    # 10.) Calculate curve metrics
+    mean_curve = np.mean(all_drop_curves, axis=0)
+    std_curve = np.std(all_drop_curves, axis=0, ddof=1)  # ddof parameter is set to 1 to return the sample std
+
+    return mean_curve, std_curve
 
